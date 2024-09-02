4486bf04d013c5c49b3b74f45f8159a456d94730
Boryana <boryana.hadzhiyska@cfa.harvard.edu>
2024-09-02 07:21:32 -0400

    added exception for model 4

--- plot_apat.py
+++ plot_apat.py
@@ -53,9 +53,9 @@ python plot_apat.py 40. 4 y
 rp_min = rt_min = 0.
 rp_max = rt_max = 200.
 r_max = 148.
-N_jk = 6 #*8
-stats_type = "stack_"
-#stats_type = "jk_"
+N_jk = 6*8
+#stats_type = "stack_"
+stats_type = "jk_"
 
 r_min = float(sys.argv[1])# 10.
 model_no = int(sys.argv[2]) #1
@@ -63,7 +63,7 @@ los_dir = (sys.argv[3]) #1
 #los_dir = "z"
 #los_dir = "y"
 
-data = np.load(f"data_fits/jk_stats_Model_1_LOSz_rpmin{rp_min:.1f}_rpmax{rp_max:.1f}_rtmin{rt_min:.1f}_rtmax{rt_max:.1f}_rmin{r_min:.1f}_rmax{r_max:.1f}_njk{N_jk:d}.npz", allow_pickle=True)
+data = np.load(f"data_fits/jk_stats_Model_{model_no:d}_LOS{los_dir}_rpmin{rp_min:.1f}_rpmax{rp_max:.1f}_rtmin{rt_min:.1f}_rtmax{rt_max:.1f}_rmin{r_min:.1f}_rmax{r_max:.1f}_njk{N_jk:d}.npz", allow_pickle=True)
 print(data.files)
 #'param_mean_error_perc_sign', 'aps', 'ats', 'bias', 'beta', 'sigmap', 'sigmat'
 
@@ -72,6 +72,16 @@ aps = data['aps']
 ats = data['ats']
 ap_mean, ap_error, ap_perc, ap_sign = param_mean_error_perc_sign['aps']
 at_mean, at_error, at_perc, at_sign = param_mean_error_perc_sign['ats']
+
+# THERE IS SOME KIND OF ISSUE WITH THE LOS_DIR = Y MODEL 4 COULD WE HAVE MESSED UP THE CALCULATION
+mask = (aps < 1.5) & (aps > 0.5) & (ats < 1.5) & (ats > 0.5)
+if "jk_" == stats_type:
+    N = np.sum(mask)
+    print(N)
+    ap_mean = np.mean(aps[mask])
+    at_mean = np.mean(ats[mask])
+    ap_error = np.sqrt(N-1.)*np.std(aps[mask], ddof=0)
+    at_error = np.sqrt(N-1.)*np.std(ats[mask], ddof=0)
 
 plt.figure(figsize=(9, 7))
 plt.scatter(aps, ats)
--- plot_rmin_apat.py
+++ plot_rmin_apat.py
@@ -46,6 +46,19 @@ for j in range(4):
         ap_mean, ap_error, ap_perc, ap_sign = param_mean_error_perc_sign['aps']
         at_mean, at_error, at_perc, at_sign = param_mean_error_perc_sign['ats']
 
+        
+        # THERE IS SOME KIND OF ISSUE WITH THE LOS_DIR = Y MODEL 4 COULD WE HAVE MESSED UP THE CALCULATION (affects only los_dir = y, model_no = 4, jk, rmin = 30, 40)
+        mask = (aps < 1.5) & (aps > 0.5) & (ats < 1.5) & (ats > 0.5)
+        if "jk_" == stats_type:
+            N = np.sum(mask)
+            print(N)
+            ap_mean = np.mean(aps[mask])
+            at_mean = np.mean(ats[mask])
+            ap_error = np.sqrt(N-1.)*np.std(aps[mask], ddof=0)
+            at_error = np.sqrt(N-1.)*np.std(ats[mask], ddof=0)
+
+
+        
         ap_means[i] = ap_mean
         at_means[i] = at_mean
         ap_errors[i] = ap_error
@@ -53,22 +66,21 @@ for j in range(4):
 
     plt.subplot(2, 1, 1)
     #plt.figure(1, figsize=(9, 7))
-    plt.errorbar(r_mins-offsets[j], ap_means, yerr=ap_errors, color=cs[j], ls='', marker='o', capsize=4, label=rf"${{\rm Model}} \ {j+1:d}$")
-
+    plt.errorbar(r_mins+offsets[j], ap_means, yerr=ap_errors, color=cs[j], ls='', marker='o', capsize=4, label=rf"${{\rm Model}} \ {j+1:d}$")
 
     plt.subplot(2, 1, 2)
     #plt.figure(2, figsize=(9, 7))
-    plt.errorbar(r_mins-offsets[j], at_means, yerr=at_errors, color=cs[j], ls='', capsize=4, marker='o')
+    plt.errorbar(r_mins+offsets[j], at_means, yerr=at_errors, color=cs[j], ls='', capsize=4, marker='o')
 
 plt.subplot(2, 1, 1)
 plt.ylabel(r"$\alpha_\parallel$")
-plt.ylim([0.985, 1.015])
+plt.ylim([0.98, 1.02])
 plt.gca().axhline(y=1, ls='--', color='k')
 plt.legend(frameon=False)
 
 plt.subplot(2, 1, 2)
 plt.ylabel(r"$\alpha_\perp$")
-plt.ylim([0.985, 1.015])
+plt.ylim([0.98, 1.02])
 plt.gca().axhline(y=1, ls='--', color='k')
 plt.savefig(f"figs/{stats_type}apat_rmin_LOS{los_dir}_njk{N_jk:d}.png")
 plt.show()
